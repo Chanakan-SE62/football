474642181d148cc721dbcdb1ffcf3435de088ef4
Chanakan-SE62 <59428866+Chanakan-SE62@users.noreply.github.com>
2022-11-28 21:07:03 +0700

    nav v.1

--- components/nav.php
+++ components/nav.php
@@ -1,28 +1,41 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar fixed-top navbar-expand-lg ">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">
+            <img src="./img/logo_football.png" alt="" width="100">
+            Football Club
+        </a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" aria-current="page" href="#">หน้าหลัก</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
+                    <a class="nav-link" href="#">การแข่งขัน</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                    <a class="nav-link" href="#">ผังการแข่งขัน</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">คะแนน</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown link
+                        แบ่งกลุ่ม
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="#">กลุ่ม A</a></li>
+                        <li><a class="dropdown-item" href="#">กลุ่ม B</a></li>
+                        <li><a class="dropdown-item" href="#">กลุ่ม C</a></li>
+                        <li><a class="dropdown-item" href="#">กลุ่ม D</a></li>
+                        <li><a class="dropdown-item" href="#">กลุ่ม E</a></li>
+                        <li><a class="dropdown-item" href="#">กลุ่ม F</a></li>
+                        <li><a class="dropdown-item" href="#">กลุ่ม G</a></li>
+                        <li><a class="dropdown-item" href="#">กลุ่ม H</a></li>
                     </ul>
                 </li>
             </ul>
